51aa9f4d42f5030b7b03ecd3bb47d46d5ce46c16
Vedant-OGC <sharonverma73@gmail.com>
2026-01-02 23:00:43 +0530

    Updated prediction

--- LR 1/prediction.py
+++ LR 1/prediction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -41,3 +42,11 @@ mse = mean_squared_error(y_test, y_pred)
 
 print("Mean Squared Error (MSE):", mse)
 
+plt.scatter(X, y, label="Actual Data")
+plt.plot(X, model.predict(X), label="Regression Line")
+plt.xlabel("Experience (Years)")
+plt.ylabel("Salary")
+plt.title("Salary vs Experience")
+#plt.figtext(0.8, 0.0,"Newton")
+plt.legend()
+plt.show()
